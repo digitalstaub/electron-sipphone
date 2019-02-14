639ed38110e3f14ca3d266f3a67d34a634aeb7a6
Benjamin <benni@digitalstaub.com>
2019-02-14 17:08:20 +0100

    bulma, redux-form

--- app/Routes.js
+++ app/Routes.js
@@ -3,12 +3,12 @@ import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import SettingsPage from './containers/SettingsPage';
 
 export default () => (
   <App>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
+      <Route path={routes.SETTINGS} component={SettingsPage} />
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
