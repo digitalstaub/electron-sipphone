acae4c6f95afc483241da1278993be1d9751da53
Benjamin <benni@digitalstaub.com>
2019-02-20 14:54:00 +0100

    redux-persist with electron storage

--- app/index.js
+++ app/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
 import { configureStore, history } from './store/configureStore';
 import './app.global.css';
 
 const store = configureStore();
+const persistor = persistStore(store);
 
 render(
   <AppContainer>
-    <Root store={store} history={history} />
+    <PersistGate loading={null} persistor={persistor}>
+      <Root store={store} history={history} />
+    </PersistGate>
   </AppContainer>,
   document.getElementById('root')
 );
@@ -20,7 +25,9 @@ if (module.hot) {
     const NextRoot = require('./containers/Root').default;
     render(
       <AppContainer>
-        <NextRoot store={store} history={history} />
+        <PersistGate loading={null} persistor={persistor}>
+          <NextRoot store={store} history={history} />
+        </PersistGate>
       </AppContainer>,
       document.getElementById('root')
     );
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,12 +2,21 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { reducer as formReducer } from 'redux-form';
+import { persistReducer } from 'redux-persist';
+import createElectronStorage from 'redux-persist-electron-storage';
 import counter from './counter';
+
+const storage = createElectronStorage();
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
 
 export default function createRootReducer(history: History) {
   return combineReducers({
     router: connectRouter(history),
     counter,
-    form: formReducer
+    form: persistReducer(persistConfig, formReducer)
   });
 }
